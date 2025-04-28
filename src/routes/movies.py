eddccd419d27625535e282c68e641f4656005142
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-04-28 20:26:07 +0300

    add /movies/{movie_id} endpoint

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -10,25 +10,43 @@ from schemas.movies import MovieDetailResponseSchema
 
 router = APIRouter()
 
+NO_MOVIES_ERROR = "No movies found."
+NO_MOVIE_ID_ERROR = "Movie with the given ID was not found."
 
-@router.get("/movies/", response_model=MovieListResponseSchema)
+@router.get(
+    "/movies/",
+    response_model=MovieListResponseSchema,
+    responses={
+        404: {
+            "content": {
+                "application/json": {"example": {"detail": NO_MOVIES_ERROR}}
+            },
+        }
+    },
+)
 async def read_movies(
-        db: AsyncSession = Depends(get_db),
-        page: int = Query(1, ge=1),
-        per_page: int = Query(10, ge=1, le=20),
+    db: AsyncSession = Depends(get_db),
+    page: int = Query(1, ge=1),
+    per_page: int = Query(10, ge=1, le=20),
 ):
     total = await db.scalar(select(func.count()).select_from(MovieModel))
     total_pages = ceil(total / per_page)
 
-    if total == 0 or page > total_pages :
-        raise HTTPException(status_code=404, detail="No movies found.")
+    if total == 0 or page > total_pages:
+        raise HTTPException(status_code=404, detail=NO_MOVIES_ERROR)
 
-    movies = await db.execute(select(MovieModel).limit(per_page).offset((page - 1) * per_page))
+    movies = await db.execute(
+        select(MovieModel).limit(per_page).offset((page - 1) * per_page)
+    )
     movies = movies.scalars().all()
-    movies = [MovieDetailResponseSchema.model_validate(movie) for movie in movies]
+    movies = [
+        MovieDetailResponseSchema.model_validate(movie) for movie in movies
+    ]
 
     url_ = "/theater/movies?page="
-    next_page = f"{url_}{page + 1}&per_page={per_page}" if page < total_pages else None
+    next_page = (
+        f"{url_}{page + 1}&per_page={per_page}" if page < total_pages else None
+    )
     prev_page = f"{url_}{page - 1}&per_page={per_page}" if page > 1 else None
 
     return MovieListResponseSchema(
@@ -38,3 +56,26 @@ async def read_movies(
         total_pages=total_pages,
         total_items=total,
     )
+
+
+@router.get(
+    "/movies/{movie_id}/",
+    response_model=MovieDetailResponseSchema,
+    responses={
+        404: {
+            "content": {
+                "application/json": {"example": {"detail": NO_MOVIE_ID_ERROR}}
+            },
+        }
+    },
+)
+async def read_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    movie = await db.execute(
+        select(MovieModel).where(MovieModel.id == movie_id)
+    )
+    movie = movie.scalars().first()
+    if not movie:
+        raise HTTPException(
+            status_code=404, detail=NO_MOVIE_ID_ERROR
+        )
+    return MovieDetailResponseSchema.model_validate(movie)
